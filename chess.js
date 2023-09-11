3be66d9174d918081fbe21e0a45386168ab0e30a
ganjela <nikaganjelashvili@gmail.com>
2023-09-11 13:49:17 +0400

    added pieces array and creatBoard() function

--- chess.js
+++ chess.js
@@ -1,3 +1,26 @@
 const chessBoard = document.querySelector("#chessboard");
 const playerColor = document.querySelector("#player-color");
 const infoDisplay = document.querySelector("info-display");
+const width = 8;
+
+const pieces = [
+    rook, knight, bishop, queen, king, bishop, knight, rook,
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+    '', '', '', '', '', '', '' , '',
+    '', '', '', '', '', '', '' , '',
+    '', '', '', '', '', '', '' , '',
+    '', '', '', '', '', '', '' , '',
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+    rook, knight, bishop, queen, king, bishop, knight, rook
+]
+
+function createBoard(){
+    pieces.forEach((piece)=>{
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.classList.add('beige');
+        chessBoard.append(square);
+    });
+}
+
+createBoard()
